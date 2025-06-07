920a87ed96fef0b61d4e100fb92f12d01e533d89
akash <akash.sid517@gmail.com>
2025-06-08 02:43:34 +0530

    filter is not working

--- stores/user.js
+++ stores/user.js
@@ -5,7 +5,7 @@ export const useUserStore = defineStore("user" , {
     state: () => ({
         text: ""
     }),
-
+           
     actions: {
         setText(newText){
             this.text = newText
